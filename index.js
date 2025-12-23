871204379a0c2cab95aa4e79461515cc9f7d6ed4
Naomi Nzioka <nziokanaomi422@gmail.com>
2025-12-23 22:59:45 +0300

    commit for lab Async Javascript-Random Social Media

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your code here!
+function displayPosts(posts) {
+    const postList = document.getElementById("post-list")
+    posts.forEach(post => {
+        const listItem = document.createElement("li");
+        const postTitle = document.createElement("h1");
+        postTitle.textContent = post.title
+        const paragraph = document.createElement("p")
+        paragraph.textContent = post.body
+        listItem.appendChild(postTitle)
+        listItem.appendChild(paragraph)
+        postList.appendChild(listItem)
+    });
+}
+
+async function getPosts() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const postsArray = await response.json();
+        displayPosts(postsArray);
+    } catch (error) {
+        console.error("Error fetching data:", error)
+    }
+}
+
+getPosts();
